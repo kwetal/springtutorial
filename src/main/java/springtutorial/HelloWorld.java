8cdb0a0d78727f8602691ed786ad92429cc135a2
Arjen Bax <arjen.bax@cgi.com>
2017-10-01 21:55:24 +0200

    add init and destroy hooks; note the registerShutdownHook!

--- src/main/java/springtutorial/HelloWorld.java
+++ src/main/java/springtutorial/HelloWorld.java
@@ -1,16 +1,31 @@
 package springtutorial;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author Arjen Bax <arjen.bax@cgi.com>
  */
-public class HelloWorld {
-   private String message;
+public class HelloWorld
+{
+    private static final Logger LOG = LogManager.getLogger("HelloWorld");
 
-   public void setMessage(String message) {
-      this.message  = message;
-   }
-   public void getMessage(){
-      System.out.println("Your Message : " + message);
-   }
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Your Message : " + message);
+    }
+
+    public void init() {
+        LOG.info("Bean is going through init.");
+    }
+
+    public void destroy() {
+        LOG.info("Bean will destroy now.");
+    }
 }
--- src/main/java/springtutorial/MainApp.java
+++ src/main/java/springtutorial/MainApp.java
@@ -1,11 +1,12 @@
 package springtutorial;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      context.registerShutdownHook();
       HelloWorld obj = (HelloWorld)context.getBean("helloWorld");
       obj.getMessage();
    }
